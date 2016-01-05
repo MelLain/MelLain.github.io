29eb16572bdf026ac03d1dfbec2af589b52cc3a8
Murat Apishev <great-mel@yandex.ru>
2016-01-05 11:15:50 +0400

    rm some files, add one new

--- politic_with_topic_names.js
+++ politic_with_topic_names.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 setParams({
-    graphFile : "navalny_no_topic_names.gexf",
+    graphFile : "politic_with_topic_names.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
